e1a8ed84b27f88ec19504bd241edeae23d17b69e
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-08-21 22:44:29 +0600

    Create Clearance Sale.cpp

---
+++ START148/Clearance Sale.cpp
@@ -0,0 +1,17 @@
+// Code-Chef Link: https://www.codechef.com/START148D/problems/CLEARANCE?tab=statement
+
+// T.C: O(1)
+// S.C: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int x;
+	cin>> x;
+	
+	cout<<x+(x/2)<<endl;
+
+}
